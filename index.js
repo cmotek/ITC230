c3304112a11a9f745362fc55cfc502bb320bbd5d
Flowzeliak <38844285+flowzeliak@users.noreply.github.com>
2019-04-24 22:04:16 -0700

    Assignment 2

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require("http"); 
 const fs = require("fs");
+const cheez = require("./cheez.js");
+let qs = require("querystring");
 http.createServer((req,res) => {
   const path = req.url.toLowerCase();
-  switch(path) {
+  const url = req.url.split("?");
+  const query = qs.parse(url[1]);
+  const bumbum = url[0];
+  switch(bumbum) {
     case '/':   
       fs.readFile("public/home.html", (err, data) => {
       if (err) return console.error(err);
@@ -13,6 +18,21 @@ http.createServer((req,res) => {
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('About page');
+      break;
+    case '/getall':
+      res.end(JSON.stringify(cheez.getAll()));
+      break;
+    case '/get':
+      let burger = cheez.get(query.name);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      let results = (burger) ? JSON.stringify(burger) : "Not found";
+      res.end(results);
+      break;
+    case '/delete':
+      let ding = cheez.get(query.name);
+      cheez.dilly(query.name);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Item Deleted ' + ding.name);
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
